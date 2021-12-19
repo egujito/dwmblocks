3b132b0049fe3f288f9d120406d0e0cca9d8a29d
saiko-2043 <bessapachecolr@gmail.com>
2021-12-19 10:36:11 +0000

    Added the scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,13 @@
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"", "~/.local/bin/statusbar/pkgs", 60, 0},
-    {"", "~/.local/bin/statusbar/ram", 5, 0},
-    {"", "~/.local/bin/statusbar/battery", 5, 0},
-    {"", "~/.local/bin/statusbar/time", 0, 0},
-    {"", "~/.local/bin/statusbar/date", 60, 0},
+    {"", "~/.config/suckless/dwmblocks/scripts/pkgs", 60, 0},
+    {"", "~/.config/suckless/dwmblocks/scripts/ram", 5, 0},
+    {"", "~/.config/suckless/dwmblocks/scripts/battery", 5, 0},
+    {"", "~/.config/suckless/dwmblocks/scripts/time", 0, 0},
+    {"", "~/.config/suckless/dwmblocks/scripts/date", 60, 0},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
 // delimeter.
-static char delim[] = "|";
+static char delim[] = " / ";
 static unsigned int delimLen = 5;
